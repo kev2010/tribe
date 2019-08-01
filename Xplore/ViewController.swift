d3476f71bb66f1798b5c57ce87bb26d9b16a7d06
Kevin Jiang <cloudwiz24@gmail.com>
2019-08-01 02:24:53 -0400

    skeleton Xcode

---
+++ Xplore/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Xplore
+//
+//  Created by Kevin Jiang on 7/31/19.
+//  Copyright © 2019 Kevin Jiang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
