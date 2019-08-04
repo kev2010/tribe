e38c8dbff9caacca9031ca571e035ef217ed199c
Baptiste Bouvier <baptistebouvier@gmail.com>
2019-08-04 11:57:19 +0200

    animations + restructuring code
    1. Not sure why the map view needs to be on a scroll view - I removed it and just added it to the regular view that comes with the view controller
    
    2. Removed the xib files and just used the storyboard directly, it’s a lot neater and uses what Xcode is good at
    
    3. Added a back button to switch from map to main screen and vice versa
    
    4. Created two new custom classes for the segues, to help us do the transitions from the left and right (“SegueFromRight” and “SegueFromLeft”). I spent quite a bit of time getting this animation right so let me know what you guys think.

--- Xplore/InteractiveMap.swift
+++ Xplore/InteractiveMap.swift
@@ -11,9 +11,13 @@ import Mapbox
 
 class InteractiveMap: UIViewController, MGLMapViewDelegate, CLLocationManagerDelegate{
     //  MapBox custom map - to change map style, go to storyboard -> MapView -> Attributes -> Style URL
-    @IBOutlet var mapView: MGLMapView!
+//    @IBOutlet var mapView: MGLMapView!
+    
+    @IBOutlet var pageView: UIScrollView!
     
     let manager = CLLocationManager()
+    
+    var mapView = MGLMapView(frame: CGRect(x: 0, y: 0, width: 0, height: 0))
 
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         //  Determine user's current location and save boundaries
@@ -24,24 +28,46 @@ class InteractiveMap: UIViewController, MGLMapViewDelegate, CLLocationManagerDel
 
         //  Display the user's region onto screen
         mapView.setVisibleCoordinateBounds(region, animated: false)
-        self.mapView.showsUserLocation = true
+        mapView.showsUserLocation = true
         print(location.speed, location.altitude)
     }
     
 
     @IBAction func toHome(_ sender: UIButton) {
         print("go back!!!")
+        self.performSegue(withIdentifier: "toMain", sender: self)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-//        backButton.addTarget(self, action: #selector(self.goBack(sender:)), for: .touchUpInside)
+        
+        // Create back  button
+        let f = CGRect(x: 10, y: 40, width: 50, height: 50)
+        let backButton = UIButton(frame: f)
+        backButton.setTitle("<", for: UIControl.State.normal)
+        backButton.setTitleColor(UIColor.black, for: UIControl.State.normal)
+        backButton.addTarget(self, action: #selector(self.goBack), for: UIControl.Event.touchDown)
+        backButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 30)
+        
+        //Load map view
+        mapView = MGLMapView(frame: CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height))
+
+        
+        //Add map and button to scroll view
+        self.view.addSubview(mapView)
+//        self.pageView.addSubview(mapView)
+        self.mapView.addSubview(backButton)
+        
         
         // Configure location manager to user's location
         manager.delegate = self
         manager.desiredAccuracy = kCLLocationAccuracyBest
         manager.requestWhenInUseAuthorization()
         manager.startUpdatingLocation()
+    }
+    
+    @objc func goBack() {
+        self.performSegue(withIdentifier: "toMain", sender: self)
     }
 
 
--- Xplore/MainScreen.swift
+++ Xplore/MainScreen.swift
@@ -11,30 +11,8 @@ import Mapbox
 
 class MainScreen: UIViewController {
     
-    //  Controls the home and map screens
-    @IBOutlet weak var pageView: UIScrollView!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        //  Initialize two screens
-        let homescreen : Home = Home(nibName: "Home", bundle: nil)
-        let mapscreen : InteractiveMap = InteractiveMap(nibName: "InteractiveMap", bundle: nil)
-        
-        //  Adding home screen
-        self.addChild(homescreen)
-        self.pageView.addSubview(homescreen.view)
-        homescreen.didMove(toParent: self)
-        //  Adding map screen
-        self.addChild(mapscreen)
-        self.pageView.addSubview(mapscreen.view)
-        mapscreen.didMove(toParent: self)
-        
-        //  Creating "scrolling" feature for the screens
-        var homeFrame : CGRect = homescreen.view.frame
-        homeFrame.origin.x = self.view.frame.width
-        homescreen.view.frame = homeFrame
-        self.pageView.contentSize = CGSize(width: self.view.frame.width*2, height: self.view.frame.height)
         
     }
     
