3e29561b2a19232f6ee4704e5bdabf4ac4364b47
Kevin Jiang <cloudwiz24@gmail.com>
2019-08-07 02:51:18 -0400

    stored new user info to Firestore

--- Xplore/Login and Signup/SignUp.swift
+++ Xplore/Login and Signup/SignUp.swift
@@ -8,43 +8,36 @@
 
 import UIKit
 import Foundation
+import Mapbox
 import Firebase
 
 class SignUp: UIViewController, UITextFieldDelegate {
     
-    
+    //  Outlets for each of the user's inputs into register screen
     @IBOutlet weak var nameField: UITextField!
     @IBOutlet weak var dobField: UITextField!
     @IBOutlet weak var emailField: UITextField!
     @IBOutlet weak var usernameField: UITextField!
     @IBOutlet weak var passwordField: UITextField!
     
-    
+    //  Set up Firestore Database
     private var datePicker: UIDatePicker!
-    
-    var docRef: DocumentReference!
-    var continueButton:RoundedWhiteButton!
-    var activityView:UIActivityIndicatorView!
+    let db = Firestore.firestore()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //  Add Background gradient
         view.addGradientLayer(topColor: UIColor(displayP3Red: 0/255, green: 255/255, blue: 179/255, alpha: 1), bottomColor: UIColor(displayP3Red: 0/255, green: 255/255, blue: 255/255, alpha: 1))
         
+        //  Set up date input
         datePicker = UIDatePicker()
         datePicker.datePickerMode = .date
         datePicker.addTarget(self, action: #selector(SignUp.dateChanged(datePicker:)), for: .valueChanged)
         
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(SignUp.viewTapped(gestureRecognizer:)))
-        
         view.addGestureRecognizer(tapGesture)
-        
         dobField.inputView = datePicker
-        
-        
-//        usernameField.delegate = self
-//        emailField.delegate = self
-//        passwordField.delegate = self
     }
     
     @objc func viewTapped(gestureRecognizer: UITapGestureRecognizer) {
@@ -62,18 +55,20 @@ class SignUp: UIViewController, UITextFieldDelegate {
         self.dismiss(animated: true, completion: nil)
     }
     
-    
-    @objc func handleSignUp() {
+    @IBAction func registerTapped(_ sender: LoginButton) {
+        //  Make sure all fields aren't empty
         guard let name = nameField.text else { return }
         guard let dob = dobField.text else { return }
         guard let email = emailField.text else { return }
         guard let username = usernameField.text else { return }
         guard let pass = passwordField.text else { return }
         
+        //  Create user on Firebase Authentication
         Auth.auth().createUser(withEmail: email, password: pass) { user, error in
             if error == nil && user != nil {
                 print("User created!")
                 
+                //  Update displayName onto Firebase Authentication (is this necessary?)
                 let changeRequest = Auth.auth().currentUser?.createProfileChangeRequest()
                 changeRequest?.displayName = username
                 
@@ -83,29 +78,22 @@ class SignUp: UIViewController, UITextFieldDelegate {
                         self.dismiss(animated: false, completion: nil)
                     } else {
                         print("Error: \(error!.localizedDescription)")
-                        self.activityView.stopAnimating()
                     }
                 }
                 
+                //  Store new user and default information into UID document
+                let uid = String(Auth.auth().currentUser!.uid)
+                let userinfo = ["status": "green", "current_event": "", "dob": dob, "email": email, "location": nil, "name": name, "private": false, "rating": 5, "username": username] as [String : Any?]
+                let socialinfo = ["friends": [], "blocked": []] as [String : Any?]
+                let eventsinfo = ["attended": [], "bookmarked": [], "hosted": [], "interested": [], "tags": [:]] as [String : Any]
+                
+                self.db.collection("users").document(uid).setData(["user_information": userinfo, "social": socialinfo, "events": eventsinfo])
+                
             } else {
                 print("Error: \(error!.localizedDescription)")
-                self.activityView.stopAnimating()
             }
-        }
         
-//        // Add a new document with a generated ID
-//        var ref: DocumentReference? = nil
-//        ref = db.collection("users").addDocument(data: [
-//            "first": "Ada",
-//            "last": "Lovelace",
-//            "born": 1815
-//        ]) { err in
-//            if let err = err {
-//                print("Error adding document: \(err)")
-//            } else {
-//                print("Document added with ID: \(ref!.documentID)")
-//            }
-//        }
+    }
 
         
     }
