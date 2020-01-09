9fa388d4ec980c5ad0a69d63a35d5ed84e7fec8a
Kevin Jiang <cloudwiz24@gmail.com>
2020-01-08 19:39:50 -0500

    updated Add Event UI

--- Xplore/AddEventPageTwoViewController.swift
+++ Xplore/AddEventPageTwoViewController.swift
@@ -13,58 +13,79 @@ class AddEventPageTwoViewController: UIViewController {
     @IBOutlet var academicTag: RoundUIView!
     @IBOutlet var artsTag: RoundUIView!
     @IBOutlet var professionalTag: RoundUIView!
-    
     @IBOutlet var athleticTag: RoundUIView!
-    
     @IBOutlet var socialTag: RoundUIView!
     @IBOutlet var casualTag: RoundUIView!
     
-    @IBOutlet var selectAddress: RoundUIView!
+    
+    @IBOutlet weak var academicLabel: UILabel!
+    @IBOutlet weak var artsLabel: UILabel!
+    @IBOutlet weak var athleticLabel: UILabel!
+    @IBOutlet weak var casualLabel: UILabel!
+    @IBOutlet weak var professionalLabel: UILabel!
+    @IBOutlet weak var socialLabel: UILabel!
+    
+    
+//    @IBOutlet var selectAddress: RoundUIView!
     
     
     @IBOutlet weak var background: RoundUIView!
     @IBOutlet weak var topUI: RoundUIView!
-    @IBOutlet weak var botUI: RoundUIView!
-    @IBOutlet weak var addressline: UIView!
+//    @IBOutlet weak var botUI: RoundUIView!
+//    @IBOutlet weak var addressline: UIView!
     
     
-    @IBOutlet var saveLabel: RoundUIView!
+//    @IBOutlet var saveLabel: RoundUIView!
     
     @IBOutlet var capacityField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let color1 = UIColor(displayP3Red: 0/255, green: 230/255, blue: 179/255, alpha: 0.6)
-        let color2 = UIColor(red: 0/255, green: 182/255, blue: 255/255, alpha: 0.6)
-        background.addGradientLayer(topColor: color1, bottomColor: color2)
+//        let color1 = UIColor(displayP3Red: 0/255, green: 230/255, blue: 179/255, alpha: 0.6)
+//        let color2 = UIColor(red: 0/255, green: 182/255, blue: 255/255, alpha: 0.6)
+//        background.addGradientLayer(topColor: color1, bottomColor: color2)
 //        addressline.addGradientLayer(topColor: color1, bottomColor: color2)
         
-        topUI.dropShadow(color: .gray, opacity: 1, offSet: CGSize(width: -1, height: 1), radius: 3, scale: true)
-        botUI.dropShadow(color: .gray, opacity: 1, offSet: CGSize(width: -1, height: 1), radius: 3, scale: true)
+//        topUI.dropShadow(color: .gray, opacity: 1, offSet: CGSize(width: -1, height: 1), radius: 3, scale: true)
+//        botUI.dropShadow(color: .gray, opacity: 1, offSet: CGSize(width: -1, height: 1), radius: 3, scale: true)
 
 
         
         let tap = UITapGestureRecognizer(target: self, action: #selector(self.academicTap))
         academicTag.addGestureRecognizer(tap)
         
-        let tap3 = UITapGestureRecognizer(target: self, action: #selector(self.saveTap))
-        saveLabel.addGestureRecognizer(tap3)
+//        let tap3 = UITapGestureRecognizer(target: self, action: #selector(self.saveTap))
+//        saveLabel.addGestureRecognizer(tap3)
         
         let tap2 = UITapGestureRecognizer(target: self, action: #selector(self.artsTap))
         artsTag.addGestureRecognizer(tap2)
         
-        let pickAddress = UITapGestureRecognizer(target: self, action: #selector(self.addressTap))
-        selectAddress.addGestureRecognizer(pickAddress)
+//        let pickAddress = UITapGestureRecognizer(target: self, action: #selector(self.addressTap))
+//        selectAddress.addGestureRecognizer(pickAddress)
         
         // Do any additional setup after loading the view.
     }
     
+    @IBAction func backTap(_ sender: UIButton) {
+        self.dismiss(animated: true)
+    }
     
-    @objc func saveTap(_ sender: UITapGestureRecognizer) {
+    
+    @IBAction func saveTap(_ sender: UIButton) {
+        UIButton.animate(withDuration: 0.3,
+                         animations: {
+                            sender.transform = CGAffineTransform(scaleX: 0.97, y: 0.955)
+        },
+                         completion: { finish in
+                            UIButton.animate(withDuration: 0.2, animations: {
+                                sender.transform = CGAffineTransform.identity
+                            })
+        })
         
         self.dismiss(animated: true, completion: {})
         (self.presentingViewController as! AddEventViewController).close()
     }
+    
     
     @objc func addressTap(_ sender: UITapGestureRecognizer) {
         
@@ -74,24 +95,28 @@ class AddEventPageTwoViewController: UIViewController {
     
     @objc func academicTap(_ sender: UITapGestureRecognizer) {
         
-        if academicTag.backgroundColor == UIColor.white {
-            academicTag.backgroundColor = UIColor(red: 0, green: 182/255, blue: 1.0, alpha: 0.6)
+        if academicTag.backgroundColor == UIColor(red: 239/255, green: 238/255, blue: 235/255, alpha: 1) {
+            academicTag.backgroundColor = UIColor(red: 49/255, green: 1.0, blue: 189/255, alpha: 1)
+            academicLabel.textColor = .white
         }
             
         else {
-            academicTag.backgroundColor = UIColor.white
+            academicTag.backgroundColor = UIColor(red: 239/255, green: 238/255, blue: 235/255, alpha: 1)
+            academicLabel.textColor = .lightGray
         }
 
     }
     
     @objc func artsTap(_ sender: UITapGestureRecognizer) {
         
-        if artsTag.backgroundColor == UIColor.white {
-            artsTag.backgroundColor = UIColor(red: 0, green: 182/255, blue: 1.0, alpha: 0.6)
+        if artsTag.backgroundColor == UIColor(red: 239/255, green: 238/255, blue: 235/255, alpha: 1) {
+            artsTag.backgroundColor = UIColor(red: 49/255, green: 1.0, blue: 189/255, alpha: 1)
+            artsLabel.textColor = .white
         }
             
         else {
-            artsTag.backgroundColor = UIColor.white
+            artsTag.backgroundColor = UIColor(red: 239/255, green: 238/255, blue: 235/255, alpha: 1)
+            artsLabel.textColor = .lightGray
         }
 
     }
