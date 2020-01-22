bcdee3bb4e4e508e8dfe46253b382b1ba7a0ac4c
Baptiste <baptiste@mit.edu>
2020-01-21 22:07:37 -0500

    big tile backend

---
+++ Xplore/BigTileViewController.swift
@@ -0,0 +1,30 @@
+//
+//  BigTileViewController.swift
+//  Xplore
+//
+//  Created by Baptiste Bouvier on 17/01/2020.
+//  Copyright © 2020 Kevin Jiang. All rights reserved.
+//
+
+import UIKit
+
+class BigTileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
