666f9402f042151733a8100a2c440bc2fb8ba783
Baptiste <baptiste@mit.edu>
2020-01-26 00:15:10 -0500

    changes to tile backend

---
+++ Xplore/ShowLocationViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ShowLocationViewController.swift
+//  Xplore
+//
+//  Created by Baptiste Bouvier on 25/01/2020.
+//  Copyright © 2020 Kevin Jiang. All rights reserved.
+//
+
+import UIKit
+
+class ShowLocationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
