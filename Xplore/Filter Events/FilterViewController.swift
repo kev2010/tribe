05085142b20348614cbb09ca86fae0341ec90fb6
Kevin Jiang <cloudwiz24@gmail.com>
2020-01-25 19:55:14 -0500

    updated filter UI

--- Xplore/Filter Events/FilterViewController.swift
+++ Xplore/Filter Events/FilterViewController.swift
@@ -8,8 +8,7 @@
 import UIKit
 import Firebase
 
-class FilterViewController: UIViewController, UITableViewDelegate, UITableViewDataSource,
-    UISearchBarDelegate {
+class FilterViewController: UIViewController {
 
     @IBAction func goBack(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
@@ -32,7 +31,7 @@ class FilterViewController: UIViewController, UITableViewDelegate, UITableViewDa
     
     @IBAction func filCasual(_ sender: Any) {
     }
-    @IBOutlet weak var filterTable: UITableView!
+//    @IBOutlet weak var filterTable: UITableView!
     
     let info = DispatchGroup()
     
@@ -54,70 +53,70 @@ class FilterViewController: UIViewController, UITableViewDelegate, UITableViewDa
     var filteredsections = [Section]()
     var sections = [Section]()
     
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return filteredsections.count
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return filteredsections[section].collapsed ? 0 : filteredsections[section].items.count
-    }
-    
-    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-        let header = tableView.dequeueReusableHeaderFooterView(withIdentifier: "header") as? FilterTable ?? FilterTable(reuseIdentifier: "header")
-        
-        header.titleLabel.text = filteredsections[section].name
-        header.arrowLabel.text = ">"
-        header.setCollapsed(filteredsections[section].collapsed)
-        
-        header.section = section
-        header.delegate = self
-        
-        return header
-    }
-    
-    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-        return 44.0
-    }
-    
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        //  Will need to adjust values later
-        return 75
-    }
-
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "filterCell", for: indexPath) as! FilterCellTableViewCell
-        let item = filteredsections[indexPath.section].items[indexPath.row]
-        cell.event = item // ???
-
-
-        return cell
-    }
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-    }
-    
-    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-        //  Check if the text is at least 2 characters
-        if searchText.count >= 2 {
-            // Rebuild the sections table - creation strategy, would deletion strategy be better?
-            filteredsections = []
-            for sec in sections {
-                var items = [Filter]()
-                for search in sec.items {
-                    if search.event?.title.range(of: searchText, options: .caseInsensitive, range: nil, locale: nil) != nil {
-                        items.append(search)
-                    }
-                }
-                if items.count > 0 {
-                    filteredsections.append(Section(name: sec.name, items: items))
-                }
-            }
-        } else {
-            filteredsections = sections
-        }
-        
-        self.filterTable.reloadData()
-    }
+//    func numberOfSections(in tableView: UITableView) -> Int {
+//        return filteredsections.count
+//    }
+//
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return filteredsections[section].collapsed ? 0 : filteredsections[section].items.count
+//    }
+//
+//    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+//        let header = tableView.dequeueReusableHeaderFooterView(withIdentifier: "header") as? FilterTable ?? FilterTable(reuseIdentifier: "header")
+//
+//        header.titleLabel.text = filteredsections[section].name
+//        header.arrowLabel.text = ">"
+//        header.setCollapsed(filteredsections[section].collapsed)
+//
+//        header.section = section
+//        header.delegate = self
+//
+//        return header
+//    }
+//
+//    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+//        return 44.0
+//    }
+//
+//    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+//        //  Will need to adjust values later
+//        return 75
+//    }
+//
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "filterCell", for: indexPath) as! FilterCellTableViewCell
+//        let item = filteredsections[indexPath.section].items[indexPath.row]
+//        cell.event = item // ???
+//
+//
+//        return cell
+//    }
+//
+//    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//    }
+//
+//    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+//        //  Check if the text is at least 2 characters
+//        if searchText.count >= 2 {
+//            // Rebuild the sections table - creation strategy, would deletion strategy be better?
+//            filteredsections = []
+//            for sec in sections {
+//                var items = [Filter]()
+//                for search in sec.items {
+//                    if search.event?.title.range(of: searchText, options: .caseInsensitive, range: nil, locale: nil) != nil {
+//                        items.append(search)
+//                    }
+//                }
+//                if items.count > 0 {
+//                    filteredsections.append(Section(name: sec.name, items: items))
+//                }
+//            }
+//        } else {
+//            filteredsections = sections
+//        }
+//
+//        self.filterTable.reloadData()
+//    }
 
     override func viewDidLoad() {
         
@@ -157,23 +156,23 @@ class FilterViewController: UIViewController, UITableViewDelegate, UITableViewDa
             self.filteredsections = self.sections
             
             //  Set up addUser UITableView and addUserSearch UISearchBar
-            self.filterTable.dataSource = self
-            self.filterTable.delegate = self
-            self.filterTable.register(FilterCellTableViewCell.self, forCellReuseIdentifier: "filterCell")
-            self.filterTable.tableFooterView = UIView()
+//            self.filterTable.dataSource = self
+//            self.filterTable.delegate = self
+//            self.filterTable.register(FilterCellTableViewCell.self, forCellReuseIdentifier: "filterCell")
+//            self.filterTable.tableFooterView = UIView()
         }
     }
 }
 
-extension FilterViewController: FilterTableDelegate {
-    
-    func toggleSection(_ header: FilterTable, section: Int) {
-        let collapsed = !filteredsections[section].collapsed
-        
-        // Toggle collapse
-        filteredsections[section].collapsed = collapsed
-        header.setCollapsed(collapsed)
-        self.filterTable.reloadSections(NSIndexSet(index: section) as IndexSet, with: .automatic)
-    }
-    
-}
+//extension FilterViewController: FilterTableDelegate {
+//
+//    func toggleSection(_ header: FilterTable, section: Int) {
+//        let collapsed = !filteredsections[section].collapsed
+//
+//        // Toggle collapse
+//        filteredsections[section].collapsed = collapsed
+//        header.setCollapsed(collapsed)
+//        self.filterTable.reloadSections(NSIndexSet(index: section) as IndexSet, with: .automatic)
+//    }
+//
+//}
